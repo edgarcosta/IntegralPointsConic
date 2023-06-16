6587a1c6e78e96b28232a21a58de7890320d798e
Edgar Costa <edgarc@mit.edu>
2023-06-16 13:58:31 -0400

    some fixes

--- IntegralPointsConic.m
+++ IntegralPointsConic.m
@@ -7,7 +7,7 @@ function MinimalSolutionSet(solutions, a, z, tau)
     // exclude solutions under tau multiplication
     O_solutions := [a*x + z*y where x,y := Explode(tup) : tup in solutions];
     pl := InfinitePlaces(Parent(z))[1];
-    tau_oo := Evaluate(q, pl);
+    tau_oo := Evaluate(tau, pl);
     excluded := [];
     for i->x in O_solutions do
         if i in excluded then continue; end if;
@@ -15,7 +15,7 @@ function MinimalSolutionSet(solutions, a, z, tau)
             if j+i in excluded then continue; end if;
             q := x/y;
             q_oo := Evaluate(q, pl);
-            sign := q1 gt 0 select 1 else -1;
+            sign := q_oo gt 0 select 1 else -1;
             // q = tau^k?
             // sign*tau1^k = q1
             k := Round(Log(tau_oo, Abs(q_oo)));
@@ -33,11 +33,12 @@ up to the action of the fundamental unit of the order associated to the conic
 }
     // standardize conic, GCD(a,b,c) = 1 and a > 0
     g := GCD(abc);
-    sign := abc[1] lt 0 select -1 else 1;
+    a, b, c := Explode(abc);
+    sign := a lt 0 select -1 else 1;
     den := sign*g;
     if den ne 1 then
         newabc := [elt div den : elt in abc];
-        newvalues := { d div den : d in values | d mod g ne 0 };
+        newvalues := Sort(SetToSequence({ d div den : d in values | d mod g eq 0 }));
         newres := $$(newabc, newvalues);
         res := AssociativeArray();
         for d in values do
@@ -71,13 +72,17 @@ up to the action of the fundamental unit of the order associated to the conic
     if Norm(tau) eq -1 then
      tau *:= tau;
     end if;
+    if Trace(tau) lt 0 then
+        tau := -tau;
+    end if;
+    // Norm(tau) eq 1, Tr(tau) > 0 => one is smaller than one, the other one is larger than 1
+    tau0, tau1 := Explode([Evaluate(tau, pl) : pl in InfinitePlaces(O)]);
+    assert tau0 gt 0 and tau1 gt 0;
 
     // (a*x + z*y)*(a*x + sigma(z)*y) = a f(x, y)
     z := -O!Roots(PolynomialRing(OF)![a*c, b, 1])[1][1]; //  we need to work over a maximal order for Factorisation
 
-    // Norm(tau) eq 1 => one is smaller than one, the other one is larger than 1
-    tau0, tau1 := Explode([Evaluate(tau, pl) : pl in InfinitePlaces(O)]);
-    assert tau0 gt 0 and tau1 gt 0;
+
     bound :=  Max(tau0, tau1);
 
     dmax := Max(Abs(Max(values)), Abs(Min(values)));
@@ -103,14 +108,17 @@ up to the action of the fundamental unit of the order associated to the conic
     //solutions := [tup : tup in rectangle | fn(tup) in values_set and Abs(a*x + phi0*y) le bound and Abs(a*x + phi1*y) le bound where x,y := Explode(tup)];
 
     time solutions1 := &cat [
-            [ elt : elt in [[x, y, Evaluate(fx, y)] : y in ybox | Abs(ax + phi0*y) le bound and Abs(ax + phi1*y) ] | elt[3] in values_set]
-            where ax := a*x where fx := Polynomial(abc) : x in xbox
+            [ elt : elt in [[x, y, Evaluate(f_givenx, y)] : y in ybox | Abs(ax + phi0*y) le bound and Abs(ax + phi1*y) le bound ] | elt[3] in values_set]
+            where ax := a*x where f_givenx := Polynomial([a*x^2,b*x,c])
+            : x in xbox
             ];
 
     // Abs(a*x + phi*y) <= B <=> a*x + phi*y < B and a*x + phi*y > -B <=> (because a > 0) x < (B - phi*y)/a and x > -(B + phi*y)/a
     xbox_giveny := func<y | [Ceiling(Max(-(bound + phi0*y)/a, -(bound + phi1*y)/a)) .. Floor(Min((bound-phi0*y)/a, (bound-phi1*y)/a))]>;
     time solutions := &cat [
-        [ elt : elt in [[x, y, Evaluate(fy, x)] : x in xbox_giveny(y)] | elt[3] in values_set] where fy := Polynomial([c,b,a]) : x in xbox];
+        [ elt : elt in [[x, y, Evaluate(f_giveny, x)] : x in xbox_giveny(y)] | elt[3] in values_set]
+        where f_giveny := Polynomial([c*y^2,b*y,a])
+        : y in ybox];
     assert solutions1 eq solutions;
 
 
